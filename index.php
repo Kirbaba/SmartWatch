257429fcef83643108ad24b64c5457ecaef2788f
Vladislav <v.kirbaba@gmail.com>
2015-08-07 10:53:01 +0300

    Start + header

---
+++ index.php
@@ -0,0 +1,46 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE">
+    </script>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+
+	<header class="header">
+		
+		
+				<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+					<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+						<h1>Smart Watch</h1>
+						<p>умные часы</p>
+					</div>
+				</div>
+				<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="header__phones">
+							<p>+38 (096) 111 11 11</p>
+							<p>+38 (096) 111 11 11</p>
+						</div>
+					</div>
+					<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+						<h4> ДОСТАВКА ПО КИЕВУ В ДЕНЬ ЗАКАЗА</h4>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+						<a href="#nowhere" class="header__but">Заказать звонок</a>
+					</div>
+				</div>
+	
+	
+		
+	</header>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
